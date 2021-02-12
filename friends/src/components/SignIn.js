dbc75fd854076b17605a49a1400f75e7d3f5d78a
ChadDiaz <chadcdiaz@gmail.com>
2021-02-12 14:36:40 -0500

    working on signin.js comp

--- friends/src/components/SignIn.js
+++ friends/src/components/SignIn.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axiosWithAuth from "../utilities/axiosWithAuth";
 
 const SignIn = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [credentials, setCredentials] = useState({
+    signIn: "",
+    password: "",
+  });
 
-export default SignIn
+  const history = useHistory();
+
+  const handleChanges = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div>
+      {/* <form>
+        <label htmlFor="signIn" />
+        <input
+          id="signIn"
+          name="signIn"
+          input="text"
+          value={credentials.signIn}
+          handleChanges={handleChanges}
+        />
+      </form> */}
+      <h1>HELLO FROM SIGNIN</h1>
+    </div>
+  );
+};
+
+export default SignIn;
