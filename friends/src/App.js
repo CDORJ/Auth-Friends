ab7e5bf361264210c7eaa4a9b9f1685c124bb134
ChadDiaz <chadcdiaz@gmail.com>
2021-02-11 21:19:28 -0500

    working on sign in form .js now

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,28 +1,45 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import PrivateRoute from './components/PrivateRoute';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
 
-import './App.css';
-import Navigation from './components/Navigation';
-import AddFriend from './components/AddFriend';
-import SignIn from './components/SignIn';
-import Friends from './components/Friends';
+import "./App.css";
+
+import Navigation from "./components/Navigation";
+import AddFriend from "./components/AddFriend";
+import SignIn from "./components/SignIn";
+import Friends from "./components/Friends";
+import styled from 'styled-components';
+
+const BackgroundDiv = styled.div`
+  margin-top: 0px;
+  height: 100vh;
+  background-image: url("https://www.rollingstone.com/wp-content/uploads/2019/09/FriendsLead.jpg");
+`;
+
+const FriendsH1 = styled.h1`
+margin: 0px;
+font-size: 40px
+`
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Switch>
-        <Route path='addFriend' render={() => <AddFriend />} />
-        <Route path='signin' render={() => <SignIn />} />
-        <PrivateRoute exact path='/protected' component={Friends} />
-        <Route exact path='/' render={() => {
-          return (
-            <div style={{ marginTop: '0px', height:'100vh', backgroundImage: 'url(https://www.rollingstone.com/wp-content/uploads/2019/09/FriendsLead.jpg)' }}>
-              <h1 style={{ margin: '0px' }}>Welcome To My Friends Site</h1>
-            </div>
-          );
-        }} />
+        <Route path="addFriend" render={() => <AddFriend />} />
+        <Route path="signin" render={() => <SignIn />} />
+        <PrivateRoute exact path="/protected" component={Friends} />
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return (
+              <BackgroundDiv>
+                <FriendsH1>Welcome To My Friends Site</FriendsH1>
+              </BackgroundDiv>
+            );
+          }}
+        />
       </Switch>
     </div>
   );
