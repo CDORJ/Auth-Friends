c7eb3a6244aaa6072df9b24ce57691d8ff1bce3d
ChadDiaz <chadcdiaz@gmail.com>
2021-02-11 20:50:14 -0500

    working on building components - background set on main home page

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
 
 import './App.css';
+import Navigation from './components/Navigation';
+import AddFriend from './components/AddFriend';
+import SignIn from './components/SignIn';
+import Friends from './components/Friends';
 
 function App() {
   return (
     <div className="App">
-      <h1>Auth Friends</h1>
+      <Navigation />
+      <Switch>
+        <Route path='addFriend' render={() => <AddFriend />} />
+        <Route path='signin' render={() => <SignIn />} />
+        <PrivateRoute exact path='/protected' component={Friends} />
+        <Route exact path='/' render={() => {
+          return (
+            <div style={{ marginTop: '0px', height:'100vh', backgroundImage: 'url(https://www.rollingstone.com/wp-content/uploads/2019/09/FriendsLead.jpg)' }}>
+              <h1 style={{ margin: '0px' }}>Welcome To My Friends Site</h1>
+            </div>
+          );
+        }} />
+      </Switch>
     </div>
   );
 }
---
+++ friends/src/components/PrivateRoute.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...props }) => {
+  const token = window.localStorage.getItem("token");
+    return <Route {...props} render={() => {
+        if (token) {
+          return <Component {...props} />
+        } else {
+            return <Redirect to='/signin' />;
+      }
+  }} />;
+};
+
+export default PrivateRoute;
