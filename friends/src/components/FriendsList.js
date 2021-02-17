e5852dc136678580d986a25b4c7a1533305682ee
sarahrosecooper <sarahrosecooperx@gmail.com>
2021-02-17 17:31:04 -0500

    Friends Component in progress

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const FriendsList = (props) => {
-  return <div>This is the friends list</div>;
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/friends")
+      .then((response) => {
+        setFriends(response.data);
+        console.log(friends);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      This is the friends list
+      {friends.map((friend) => {
+        return <div key={friend.id}>{friend.name}</div>;
+      })}
+    </div>
+  );
 };
 
 export default FriendsList;
